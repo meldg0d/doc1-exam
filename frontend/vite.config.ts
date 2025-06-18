f449ab38b0fb530fe46aa611e0c7a32b3cb7e5f9
Meldgod <mankangodt@gmail.com>
2025-06-19 01:12:08 +0200

    fixed css

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig, mergeConfig } from 'vite'
 import { defineConfig as defineVitestConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
 
 const viteConfig = defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
 
 const vitestConfig = defineVitestConfig({
